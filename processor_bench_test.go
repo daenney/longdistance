d08a0b4943a501b879ecad43ec79507bbe40d356
Daenney <git@noreply.sourcery.dny.nu>
2026-09-06 20:46:37 +0200

    refactor: split out the benchmarks
    Having the cached and uncached as sub-benchmarks makes tools like benchstat
    difficult to use as things like the geomean no longer mean anything.

--- processor_bench_test.go
+++ processor_bench_test.go
@@ -11,116 +11,144 @@ import (
 
 func BenchmarkContextProcessing(b *testing.B) {
 	ctx := json.RawMessage(`"https://www.w3.org/ns/activitystreams"`)
-	b.Run("without processed context", func(b *testing.B) {
-		b.ReportAllocs()
-		b.SetBytes(int64(len(ctx)))
 
-		p := ld.NewProcessor(
-			ld.WithRemoteContextLoader(StaticLoader(b, "as.jsonld")),
-		)
-
-		for b.Loop() {
-			_, err := p.Context(b.Context(), bytes.NewReader(ctx), "")
-			if err != nil {
-				b.Fatal(err)
-			}
-		}
-	})
-
-	b.Run("with processed context", func(b *testing.B) {
-		b.ReportAllocs()
-		b.SetBytes(int64(len(ctx)))
-
-		p := ld.NewProcessor(
-			ld.WithProcessedContext(ASURL, ProcessContext(b, LoadData(b, "as.jsonld"), ASURL)),
-		)
-
-		for b.Loop() {
-			_, err := p.Context(b.Context(), bytes.NewReader(ctx), "")
-			if err != nil {
-				b.Fatal(err)
-			}
-		}
-	})
-}
-
-func BenchmarkCompact(b *testing.B) {
-	doc := LoadData(b, "observatory/createnote.json")
+	b.ReportAllocs()
+	b.SetBytes(int64(len(ctx)))
 
 	p := ld.NewProcessor(
 		ld.WithRemoteContextLoader(StaticLoader(b, "as.jsonld")),
 	)
 
-	exp, err := p.Expand(b.Context(), bytes.NewReader(doc), "")
-	if err != nil {
-		b.Fatal(err)
+	for b.Loop() {
+		_, err := p.Context(b.Context(), bytes.NewReader(ctx), "")
+		if err != nil {
+			b.Fatal(err)
+		}
 	}
+}
 
+func BenchmarkContextProcessingCached(b *testing.B) {
+	ctx := json.RawMessage(`"https://www.w3.org/ns/activitystreams"`)
+
+	b.ReportAllocs()
+	b.SetBytes(int64(len(ctx)))
+
+	p := ld.NewProcessor(
+		ld.WithProcessedContext(ASURL, ProcessContext(b, LoadData(b, "as.jsonld"), ASURL)),
+	)
+
+	for b.Loop() {
+		_, err := p.Context(b.Context(), bytes.NewReader(ctx), "")
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkCompact(b *testing.B) {
 	compCtx := LoadData(b, "observatory/context.jsonld")
+	var dst bytes.Buffer
+	var exp []ld.Node
 
-	b.Run("without processed context", func(b *testing.B) {
-		b.ReportAllocs()
-
+	{
 		p := ld.NewProcessor(
 			ld.WithRemoteContextLoader(StaticLoader(b, "as.jsonld")),
 		)
 
-		for b.Loop() {
-			err := p.Compact(b.Context(), io.Discard, compCtx, exp, "")
-			if err != nil {
-				b.Fatal(err)
-			}
+		var err error
+		exp, err = p.Expand(b.Context(), bytes.NewReader(LoadData(b, "observatory/createnote.json")), "")
+		if err != nil {
+			b.Fatal(err)
 		}
-	})
 
-	b.Run("with processed context", func(b *testing.B) {
-		b.ReportAllocs()
+		if err := p.Compact(b.Context(), &dst, compCtx, exp, ""); err != nil {
+			b.Fatal(err)
+		}
+	}
 
+	b.ReportAllocs()
+	b.SetBytes(int64(dst.Len()))
+
+	p := ld.NewProcessor(
+		ld.WithRemoteContextLoader(StaticLoader(b, "as.jsonld")),
+	)
+
+	for b.Loop() {
+		err := p.Compact(b.Context(), io.Discard, compCtx, exp, "")
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkCompactCached(b *testing.B) {
+	compCtx := LoadData(b, "observatory/context.jsonld")
+	var dst bytes.Buffer
+	var exp []ld.Node
+
+	{
 		p := ld.NewProcessor(
 			ld.WithProcessedContext(ASURL, ProcessContext(b, LoadData(b, "as.jsonld"), ASURL)),
 		)
 
-		for b.Loop() {
-			err := p.Compact(b.Context(), io.Discard, compCtx, exp, "")
-			if err != nil {
-				b.Fatal(err)
-			}
+		var err error
+		exp, err = p.Expand(b.Context(), bytes.NewReader(LoadData(b, "observatory/createnote.json")), "")
+		if err != nil {
+			b.Fatal(err)
 		}
-	})
+
+		if err := p.Compact(b.Context(), &dst, compCtx, exp, ""); err != nil {
+			b.Fatal(err)
+		}
+	}
+
+	b.ReportAllocs()
+	b.SetBytes(int64(dst.Len()))
+
+	p := ld.NewProcessor(
+		ld.WithProcessedContext(ASURL, ProcessContext(b, LoadData(b, "as.jsonld"), ASURL)),
+	)
+
+	for b.Loop() {
+		err := p.Compact(b.Context(), io.Discard, compCtx, exp, "")
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
 }
 
 func BenchmarkExpand(b *testing.B) {
 	doc := LoadData(b, "observatory/createnote.json")
 
-	b.Run("without processed context", func(b *testing.B) {
-		b.ReportAllocs()
-		b.SetBytes(int64(len(doc)))
+	b.ReportAllocs()
+	b.SetBytes(int64(len(doc)))
 
-		p := ld.NewProcessor(
-			ld.WithRemoteContextLoader(StaticLoader(b, "as.jsonld")),
-		)
+	p := ld.NewProcessor(
+		ld.WithRemoteContextLoader(StaticLoader(b, "as.jsonld")),
+	)
 
-		for b.Loop() {
-			_, err := p.Expand(b.Context(), bytes.NewReader(doc), "")
-			if err != nil {
-				b.Fatal(err)
-			}
+	for b.Loop() {
+		_, err := p.Expand(b.Context(), bytes.NewReader(doc), "")
+		if err != nil {
+			b.Fatal(err)
 		}
-	})
+	}
+}
 
-	b.Run("with processed context", func(b *testing.B) {
-		b.ReportAllocs()
-		b.SetBytes(int64(len(doc)))
+func BenchmarkExpandCached(b *testing.B) {
+	doc := LoadData(b, "observatory/createnote.json")
 
-		p := ld.NewProcessor(
-			ld.WithProcessedContext(ASURL, ProcessContext(b, LoadData(b, "as.jsonld"), ASURL)),
-		)
+	b.ReportAllocs()
+	b.SetBytes(int64(len(doc)))
 
-		for b.Loop() {
-			_, err := p.Expand(b.Context(), bytes.NewReader(doc), "")
-			if err != nil {
-				b.Fatal(err)
-			}
+	p := ld.NewProcessor(
+		ld.WithProcessedContext(ASURL, ProcessContext(b, LoadData(b, "as.jsonld"), ASURL)),
+	)
+
+	for b.Loop() {
+		_, err := p.Expand(b.Context(), bytes.NewReader(doc), "")
+		if err != nil {
+			b.Fatal(err)
 		}
-	})
+	}
 }
